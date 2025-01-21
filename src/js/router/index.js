d4e4028f3d49333cf5dd5ed465ae11b96b6c82c0
Truls Johan Jacobsen-Lamøy <truls@Trulss-MBP.home>
2025-01-21 21:29:08 +0100

    chore: test for routing

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -1,8 +1,10 @@
 export default async function router(pathname = window.location.pathname) {
-    console.log(pathname)
     switch (pathname) {
         case '/':
             await import('./views/home.js');
+            break;
+        case '/auth/login/index.html':
+            await import('./views/login.js');
             break;
         default:
             await import('./views/notFound.js');
