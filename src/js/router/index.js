bc75efb957e190e5e46c1bd9396d412ab1c677b7
Truls Johan Jacobsen-Lamøy <truls@Trulss-MacBook-Pro.local>
2025-01-26 23:29:34 +0100

    chore: changes pathing

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -1,22 +1,22 @@
 export default async function router(pathname = window.location.pathname) {
     switch (pathname) {
         case '/index.html':
-            await import('./views/home.js');
+            await import('../views/home.js');
             break;
         case '/auth/register/index.html':
-            await import('./views/register.js');
+            await import('../views/register.js');
             break;
         case '/auth/login/index.html':
-            await import('./views/login.js');
+            await import('../views/login.js');
             break;
         case '/profile/index.html':
-            await import('./views/profile.js');
+            await import('../views/profile.js');
             break;
         case '/post/create/index.html':
-            await import('./views/create.js');
+            await import('../views/create.js');
             break;
         case '/post/update/index.html':
-            await import('./views/update.js');
+            await import('../views/update.js');
             break;
         default:
             await import('./views/notFound.js');
