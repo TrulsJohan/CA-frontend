793bca20ddac1670fa8cedaacd7a8aa56481a766
Truls Johan Jacobsen-Lamøy <truls@Trulss-MacBook-Pro.local>
2025-01-26 23:48:39 +0100

    chore: test

---
+++ js/home.js
@@ -0,0 +1,77 @@
+import { deleteMovie } from '../../api/delete.js';
+import { API_URL } from '../global/headers.js';
+
+export async function getMovies() {
+    try {
+        const res = await fetch(`${API_URL}/movies`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+        });
+        if (!res.ok) {
+            throw new Error(`Failed to fetch posts: ${res.status}`);
+        }
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching movies:', error);
+    }
+}
+
+const moviesContainer = document.getElementById('moviesContainer');
+
+async function displayMovies() {
+    try {
+        const data = await getMovies();
+        if (!data || !data.data || data.data.length === 0) {
+            throw new Error('No movies found');
+        }
+        console.log(data);
+        moviesContainer.innerHTML = '';
+        const movies = data.data
+            .map((movie) => {
+                return `
+                <div class="movie">
+                    <h3>${movie.title}</h3>
+                    <p>${movie.description}</p>
+                    <img src="${movie.img_url}" alt="${movie.title}" style="width: 100px; height: 100px;"/>
+                    <button class="deleteBtn" data-id="${movie.id}">Delete Movie</button>
+                    <button class="editBtn" data-id="${movie.id}">Edit Movie</button>
+                </div>
+            `;
+            })
+            .join('');
+        moviesContainer.innerHTML = movies;
+        const deleteBtn = document.querySelectorAll('.deleteBtn');
+        deleteBtn.forEach((button) => {
+            button.addEventListener('click', () => {
+                const id = button.getAttribute('data-id');
+                if (id) {
+                    deleteMovie(id);
+                    /*localStorage.setItem('movie_id', id);
+                    console.log(`Movie ID ${id} saved to localStorage.`);*/
+                } else {
+                    console.error('Movie ID not found.');
+                }
+            });
+        });
+        const editBtn = document.querySelectorAll('.editBtn');
+        editBtn.forEach((button) => {
+            button.addEventListener('click', () => {
+                const id = button.getAttribute('data-id');
+                if (id) {
+                    localStorage.setItem('movie_id', id);
+                    window.location.href = '../../../../post/update/index.html';
+                } else {
+                    console.error('Movie ID not found.');
+                }
+            });
+        });
+    } catch (error) {
+        console.error('Error fetching movies:', error);
+    }
+}
+
+displayMovies();
---
+++ js/login.js
@@ -0,0 +1,30 @@
+import { API_URL } from '../global/headers.js';
+
+export async function login() {
+    try {
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        const res = await fetch(`${API_URL}/login`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password }),
+        });
+        if (!res.ok) {
+            throw new Error(`Login failed: ${res.status} ${res.statusText}`);
+        }
+        const data = await res.json();
+        if (!data.accessToken) {
+            throw new Error('Login response does not contain an access token.');
+        }
+        console.log(data);
+        localStorage.setItem('token', data.accessToken);
+        localStorage.setItem('user_id', data.user_id);
+    } catch (error) {
+        console.error('Could not log in:', error.message);
+    }
+}
+
+document.getElementById('loginForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+    login();
+});
