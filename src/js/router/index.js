dc3fca5cec7e0b31ad5b21cc561ce789603fa9c5
Truls Johan Jacobsen-Lamøy <truls@Trulss-MacBook-Pro.local>
2025-01-26 23:32:20 +0100

    chore: added src to path

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -1,24 +1,24 @@
 export default async function router(pathname = window.location.pathname) {
     switch (pathname) {
         case '/index.html':
-            await import('../views/home.js');
+            await import('/src/views/home.js');
             break;
         case '/auth/register/index.html':
-            await import('../views/register.js');
+            await import('/src/views/register.js');
             break;
         case '/auth/login/index.html':
-            await import('../views/login.js');
+            await import('/src/views/login.js');
             break;
         case '/profile/index.html':
-            await import('../views/profile.js');
+            await import('/src/views/profile.js');
             break;
         case '/post/create/index.html':
-            await import('../views/create.js');
+            await import('/src/views/create.js');
             break;
         case '/post/update/index.html':
-            await import('../views/update.js');
+            await import('/src/views/update.js');
             break;
         default:
-            await import('./views/notFound.js');
+            await import('/src/views/notFound.js');
     }
 }
