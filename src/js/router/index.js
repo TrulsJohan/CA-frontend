4112320eec6f6e1e779a5ab17d6596c632b34340
Truls Johan Jacobsen-Lamøy <truls@Trulss-MacBook-Pro.local>
2025-01-26 23:35:03 +0100

    chore: added try catch to router

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -1,24 +1,28 @@
 export default async function router(pathname = window.location.pathname) {
-    switch (pathname) {
-        case '/index.html':
-            await import('/src/views/home.js');
-            break;
-        case '/auth/register/index.html':
-            await import('/src/views/register.js');
-            break;
-        case '/auth/login/index.html':
-            await import('/src/views/login.js');
-            break;
-        case '/profile/index.html':
-            await import('/src/views/profile.js');
-            break;
-        case '/post/create/index.html':
-            await import('/src/views/create.js');
-            break;
-        case '/post/update/index.html':
-            await import('/src/views/update.js');
-            break;
-        default:
-            await import('/src/views/notFound.js');
+    try {
+        switch (pathname) {
+            case '/index.html':
+                await import('../views/home.js');
+                break;
+            case '/auth/register/index.html':
+                await import('../views/register.js');
+                break;
+            case '/auth/login/index.html':
+                await import('../views/login.js');
+                break;
+            case '/profile/index.html':
+                await import('../views/profile.js');
+                break;
+            case '/post/create/index.html':
+                await import('../views/create.js');
+                break;
+            case '/post/update/index.html':
+                await import('../views/update.js');
+                break;
+            default:
+                await import('../views/notFound.js');
+        }
+    } catch (error) {
+        console.error(`Error loading module for ${pathname}:`, error);
     }
 }
